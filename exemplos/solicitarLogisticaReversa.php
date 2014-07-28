957a0d144748e35a15d22d4082d23fc270edb2f5
Renayo S Moreno <renatomorenocz@gmail.com>
2014-07-28 08:30:54 -0300

    Arquivo de Exemplo

--- exemplos/solicitarLogisticaReversa.php
+++ exemplos/solicitarLogisticaReversa.php
@@ -1,65 +1,46 @@
-<?php
-require_once __DIR__ . '/bootstrap-exemplos.php';
+<?php 
 
-$accessDataDeHomologacao = new \PhpSigep\Model\AccessDataHomologacao();
-$usuario = trim((isset($_GET['usuario']) ? $_GET['usuario'] : $accessDataDeHomologacao->getUsuario()));
-$senha = trim((isset($_GET['senha']) ? $_GET['senha'] : $accessDataDeHomologacao->getSenha()));
+// Altera as configurações do PHP para mostrar todos os erros, já que este é apenas um script de exemplo.
+// No seu ambiente de produção, você não vai precisar alterar estas configurações.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ini_set('error_reporting', 'E_ALL|E_STRICT');
+error_reporting(E_ALL);
 
-$accessData = new \PhpSigep\Model\AccessData();
-$accessData->setUsuario($usuario);
-$accessData->setSenha($senha);
+header('Content-Type: text/html; charset=utf-8');
 
-$params = new \PhpSigep\Model\SolicitaEtiquetas();
-$params->setQtdEtiquetas(1);
-$params->setServicoDePostagem(\PhpSigep\Model\ServicoDePostagem::SERVICE_E_SEDEX_STANDARD);
-$params->setAccessData($accessData);
+// Configura o php-sigep
 
-$phpSigep = new PhpSigep\Services\SoapClient\Real();
+// Se você não usou o composer é necessário carregar o script Boostrap.php manualmente.
+// Caso você tenha usado o composer o Bootstrap PHP será carregado automáticamente pelo autoload (quando necessário).
+//require_once __DIR__ . '/../src/PhpSigep/Bootstrap.php';
+require_once __DIR__. '/../vendor/autoload.php';
 
-echo <<<HTML
-    <div style="display:inline-block;color: red;font-weight: bold;border: 1px solid silver;padding: 20px;background-color: #fffcfc">
-        O Correios sempre retorna "A autenticacao de sigep falhou!" quando solicitamos etiquetas
-        <strong>usando o servidor de homologação</strong>.
-    </div>
-HTML;
+$accessDataParaAmbienteDeHomologacao = new \PhpSigep\Model\AccessData(array(
+                'usuario'           => '60618043',
+                'senha'             => '8o8otn',
+                'codAdministrativo' => '08082650',
+                'numeroContrato'    => '9912208555',
+                'cartaoPostagem'    => '0057018901',
+                'cnpjEmpresa'       => null, // Não consta no manual.
+                'anoContrato'       => null, // Não consta no manual.
+                'diretoria'         => new Diretoria(Diretoria::DIRETORIA_DR_RIO_DE_JANEIRO), // Não consta no manual, mas precisamos setar um valor para conseguir imprimir as etiquetas.
+            ));
 
-var_dump($phpSigep->solicitaEtiquetas($params));
+$config = new \PhpSigep\Config();
+$config->setAccessData($accessDataParaAmbienteDeHomologacao);
+$config->setEnv(\PhpSigep\Config::ENV_DEVELOPMENT);
+$config->setCacheOptions(
+    array(
+        'storageOptions' => array(
+            // Qualquer valor setado neste atributo será mesclado ao atributos das classes 
+            // "\PhpSigep\Cache\Storage\Adapter\AdapterOptions" e "\PhpSigep\Cache\Storage\Adapter\FileSystemOptions".
+            // Por tanto as chaves devem ser o nome de um dos atributos dessas classes.
+            'enabled' => false,
+            'ttl' => 10,// "time to live" de 10 segundos
+            'cacheDir' => sys_get_temp_dir(), // Opcional. Quando não informado é usado o valor retornado de "sys_get_temp_dir()"
+        ),
+    )
+);
 
-?>
-<!doctype html>
-<html lang="pt">
-    <head>
-        <meta charset="UTF-8">
-        <title>Exemplo Rastrear Objetos - PHP Sigep</title>
-    </head>
-    <body>
-        <div style="display:inline-block;color: red;font-weight: bold;border: 1px solid silver;padding: 20px;background-color: #fffcfc">
-            O Correios sempre retorna "A autenticacao de sigep falhou!" quando solicitamos etiquetas
-            <u>usando o servidor de homologação</u>.
-            <br/>
-            Por esse motivo, você deve informar um usuário e senha real para este exemplo funcionar corretamente.
-        </div>
-        <br/>
-        <br/>
-        <form action="" method="get">
-            O usuário e senha abaixo são do ambiente de homologação.
-            <br/>
-            Troque estes dados por um usuário e senha real para, caso contrario o Correios responderá que não foi possível autentificar o usuário.
-            <br/>
-            <label for="usuario">Usuário</label>
-            <input type="text" name="usuario" value="<?php echo htmlspecialchars($accessData->getUsuario(), ENT_QUOTES); ?>"/>
-            <label for="senha">Senha</label>
-            <input type="text" name="senha" value="<?php echo htmlspecialchars($accessData->getSenha(), ENT_QUOTES); ?>"/>
-            <br/>
-            <br/>
-            <label for="etiquetas">Etiquetas - separadas por vírgula</label>
-            <br/>
-            <textarea name="etiquetas" id="etiquetas" cols="60" rows="3"><?php echo htmlspecialchars($etiquetasFromQueryRaw); ?></textarea>
-            <button type="submit">Rastrear</button>
-        </form>
-        <br/>
-        <h1>Resposta</h1>
-        <hr/>
-        <?php echo $dumpResult ?>
-    </body>
-</html>
+\PhpSigep\Bootstrap::start($config);
